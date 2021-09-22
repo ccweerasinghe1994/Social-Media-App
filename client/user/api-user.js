f8ae76cf7b6245b847058a98d69a819561255459
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-09-22 19:46:30 +0530

    adding list and create user methods

--- client/user/api-user.js
+++ client/user/api-user.js
@@ -1,3 +1,9 @@
+/**
+ * Create a single new user
+ * @async create
+ * @param {object} user -user details
+ * @returns {object|void} - either returns the created user or console log the error
+ */
 const create = async (user) => {
   try {
     let response = await fetch("/api/users/", {
@@ -9,6 +15,18 @@ const create = async (user) => {
       body: JSON.stringify(user),
     });
 
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const list = async (signal) => {
+  try {
+    let response = await fetch("/api/users", {
+      method: "GET",
+      signal: signal,
+    });
     return await response.json();
   } catch (error) {
     console.log(error);
